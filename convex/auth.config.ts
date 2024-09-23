ddefe14dd75c2f46679b8e809f23a8e7e01f53f5
ajithkumar110302 <ajithkumar110302@gmail.com>
2024-09-23 20:15:10 +0530

    Initial commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://chief-gopher-82.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
+
+export default authConfig;
